b9cc68f4e4055947b8d4585c1bdf56a06b205f47
Lukáš Hozda <luk.hozda@gmail.com>
2026-07-22 19:47:55 +0200

    Release Autolith 0.13.0

--- src/configuration.lisp
+++ src/configuration.lisp
@@ -2,7 +2,7 @@
 
 ;;;; -- Defaults --
 
-(define-constant +autolith-version+ "0.12.0"
+(define-constant +autolith-version+ "0.13.0"
   :test #'string=
   :documentation "The user-visible Autolith version.")
 
